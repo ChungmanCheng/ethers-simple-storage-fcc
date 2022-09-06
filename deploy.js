18fdbe721574764d4dd575098751043c7bf183d9
ChungManCheng <erwinchengchungman916@gmail.com>
2022-09-06 16:13:44 +0800

    Added environment variables

--- deploy.js
+++ deploy.js
@@ -22,25 +22,28 @@
 const ethers = require("ethers");
 const fs = require("fs-extra");
 
+require("dotenv").config();
+
 async function main() {
+
     // deploy a contract? Wait for it to be deployed
     // contract.deploy -> wouldn't wait for it to finish
     const provider = new ethers.providers.JsonRpcProvider("http://127.0.0.1:7545");
-    const wallet = new ethers.Wallet(
-        "c9ea7ca1fe58cc8fbdb975b3ff12acb2151bb639ca9f126c3727db915b719ba5",
-        provider
-    );
+    const wallet = new ethers.Wallet(process.env.PRIVATE_KEY, provider);
     const abi = fs.readFileSync("./SimpleStorage_sol_SimpleStorage.abi", "utf8");
     const binary = fs.readFileSync("./SimpleStorage_sol_SimpleStorage.bin", "utf-8");
 
     const contractFactory = new ethers.ContractFactory(abi, binary, wallet);
     console.log("Deploying, please wait...");
     const contract = await contractFactory.deploy(); // STOP here! Wait for contract to deploy
+    await contract.deployTransaction.wait(1);
+
+    const currentFavoriteNumber = await contract.retrieve();
+    console.log(`Current Favorite: ${currentFavoriteNumber.toString()}`);
+    const transactionResponse = await contract.store("7");
     const transactionReceipt = await contract.deployTransaction.wait(1);
-    console.log("Here is the deployment transaction: ");
-    console.log(contract.deployTransaction);
-    console.log("Here is the transaction receipt: ");
-    console.log(transactionReceipt);
+    const updatedFavoriteNumber = await contract.retrieve();
+    console.log(`Updated favorite number is: ${updatedFavoriteNumber}`);
 
 }
 
