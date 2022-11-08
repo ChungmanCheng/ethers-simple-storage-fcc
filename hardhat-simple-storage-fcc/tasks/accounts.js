47e122812f8f1aeed8c8f75660cb424fb8bdca1c
erwin <erwinchengchungman916@gmail.com>
2022-11-08 11:02:01 +0800

    Merge hardhat-simple-storage-fcc into main

---
+++ hardhat-simple-storage-fcc/tasks/accounts.js
@@ -0,0 +1,9 @@
+const { task } = require("hardhat/config");
+
+task("accounts", "Prints the list of accounts", async (taskArgs, hre) => {
+    const accounts = await hre.ethers.getSigners();
+
+    for (const account of accounts) {
+        console.log(account.address);
+    }
+});
---
+++ hardhat-simple-storage-fcc/tasks/block-number.js
@@ -0,0 +1,10 @@
+const { task } = require("hardhat/config");
+
+task("block-number", "Prints the current block number").setAction(
+    // const blockTask = async functionn() => {}
+    // async function blockTask(){}
+    async (taskArgs, hre) => {
+        const blockNumber = await hre.ethers.provider.getBlockNumber();
+        console.log(`Current block number: ${blockNumber}`);
+    }
+);
